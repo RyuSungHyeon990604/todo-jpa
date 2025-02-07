55f33bc8a6e1f82a98bdb625725ca7dadfbe2518
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-07 20:48:56 +0900

    feat : todo controller 추가
    기본적인 crud 처리

---
+++ src/main/java/com/example/todojpa/controller/TodoController.java
@@ -0,0 +1,63 @@
+package com.example.todojpa.controller;
+
+
+import com.example.todojpa.dto.TodoCreateRequestDto;
+import com.example.todojpa.dto.TodoResponse;
+import com.example.todojpa.dto.TodoUpdateRequestDto;
+import com.example.todojpa.service.TodoService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+
+@RestController
+@RequestMapping("/todos")//복수형으로 작성하기
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<TodoResponse> search(@RequestParam(value = "username", required = false) String userName,
+                                               @RequestParam(value = "date", required = false) LocalDate date,
+                                               @RequestParam(value = "page", required = false) Integer page,
+                                               @RequestParam(value = "size", required = false) Integer size) {
+        TodoResponse todos = todoService.search(userName, date, PageRequest.of(page, size));
+
+        return ResponseEntity.ok(todos);
+    }
+
+    //n+1발생
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoResponse> findTodoById(@PathVariable Long id) {
+        TodoResponse todos = todoService.findTodoById(id);
+        return ResponseEntity.ok(todos);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<TodoResponse> createTodo(@RequestBody TodoCreateRequestDto requestDto,
+                                                   @RequestHeader Long userId) {
+        TodoResponse todo = todoService.createTodo(requestDto, userId);
+        return ResponseEntity.ok(todo);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updateTodo(@PathVariable Long id,
+                                           @RequestBody TodoUpdateRequestDto requestDto,
+                                           @RequestHeader Long userId) {
+        todoService.updateTodo(requestDto,id,userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    //post delete, soft delete 써보기
+    @PostMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id, @RequestHeader Long userId) {
+        todoService.deleteTodo(id, userId);
+        return ResponseEntity.noContent().build();
+    }
+}
