036046e0aad064b56cabbfbcf319a5fce98e0fbf
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-07 20:37:31 +0900

    fix : getter 추가
    직렬화 문제

--- src/main/java/com/example/todojpa/dto/CommentResponse.java
+++ src/main/java/com/example/todojpa/dto/CommentResponse.java
@@ -3,7 +3,9 @@ package com.example.todojpa.dto;
 import com.example.todojpa.entity.Comment;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentResponse {
     private String comment;
--- src/main/java/com/example/todojpa/dto/TodoDetail.java
+++ src/main/java/com/example/todojpa/dto/TodoDetail.java
@@ -3,10 +3,12 @@ package com.example.todojpa.dto;
 import com.example.todojpa.entity.Todo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoDetail {
     private Long id;
