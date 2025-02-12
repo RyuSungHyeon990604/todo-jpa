207c4920e9ad4ee22c78c11124834a3bd3d55d12
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-02-12 17:05:39 +0900

    fix : 명시적으로 left join

--- src/main/java/com/example/todojpa/repository/comment/CommentRepository.java
+++ src/main/java/com/example/todojpa/repository/comment/CommentRepository.java
@@ -11,7 +11,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 @Query(" select c " +
         "  from Comment c " +
         "  left join fetch c.user u " + //삭제된 사용자도 가져오도록, toOne 관계에서는 fetch 써도됨
-        "  left join  c.todo t " + // 삭제된 일정도 가져오도록 left join
+        "  left join Todo t " +
+        "         on c.todo = t " + // 삭제된 일정도 가져오도록 left join
         " where c.todo.id = :todoId " +
         " order by c.level asc, c.createdAt desc")//댓글은 생성일 순서로 정렬한다
     List<Comment> findAllByTodoIdOrderByLevelAscCreatedAtDesc(Long todoId);
